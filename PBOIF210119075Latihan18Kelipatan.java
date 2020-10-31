716f75f45084db00074bc36d67cf4a007ff56d03
aditmk1 <73748815+aditmk1@users.noreply.github.com>
2020-10-31 21:43:49 +0700

    Add files via upload

---
+++ PBOIF210119075Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119075.latihan18.kelipatan;
+
+/**
+ * @author  
+ * Nama  : Aditiya Musthafa Kamil
+ * Kelas : PBOIF2
+ * NIM   : 10119075
+ * Deskripsi Program : Program ini berisi program yang menampilkan Program Kelipatan
+ **/
+public class PBOIF210119075Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float bilangan;
+        for(bilangan=1;bilangan<=70;bilangan++)
+            if(bilangan%3.5==0)
+                System.out.println(bilangan*0.5);
+        System.out.println("===============================================");
+        System.out.println("Developed by Aditiya Musthafa Kamil");
+    }
+    
+}
